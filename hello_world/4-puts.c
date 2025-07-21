f4ae9f131973f9c12df3b659f9e4c7365534b6eb
natahaciyeva <natiagadjieva@gmail.com>
2025-07-21 21:37:16 +0400

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,12 @@
-gcc main.c -o puzzle
-./puzzle
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    puts("Programming is like building a multilingual puzzle");
+    return (0);
+}
