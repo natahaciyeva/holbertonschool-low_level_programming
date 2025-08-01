630f9b50c29e82fb0224190a7a5d51fa71d034e3
natahaciyeva <natiagadjieva@gmail.com>
2025-08-01 11:39:41 +0000

    4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -6,20 +6,18 @@
  */
 void print_rev(char *s)
 {
-    int len = 0;
-    int i;
+	int len = 0;
+	int i;
 
-    /* Find length of string */
-    while (s[len] != '\0')
-    {
-        len++;
-    }
+	while (s[len] != '\0')
+	{
+		len++;
+	}
 
-    /* Print characters in reverse */
-    for (i = len - 1; i >= 0; i--)
-    {
-        _putchar(s[i]);
-    }
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
 
-    _putchar('\n');
+	_putchar('\n');
 }
